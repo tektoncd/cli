a09dd8e4ecc7f0c68c2059633f7470560a65a72e
praveen4g0 <37117046+praveen4g0@users.noreply.github.com>
2019-09-30 17:03:21 +0530

    fixed ci break

--- test/e2e/pipeline_e2e_test.go
+++ test/e2e/pipeline_e2e_test.go
@@ -604,8 +604,8 @@ func getCreateFileTask(taskname string, namespace string) *v1alpha1.Task {
 		tb.TaskInputs(tb.InputsResource("workspace", v1alpha1.PipelineResourceTypeGit, tb.ResourceTargetPath("damnworkspace"))),
 		tb.TaskOutputs(tb.OutputsResource("workspace", v1alpha1.PipelineResourceTypeGit)),
 		tb.Step("read-docs-old", "ubuntu", tb.StepCommand("/bin/bash"), tb.StepArgs("-c", "ls -la /workspace/damnworkspace/docs/README.md")),
-		//tb.Step("write-new-stuff", "ubuntu", tb.StepCommand("bash"), tb.StepArgs("-c", "ln -s /workspace/damnworkspace /workspace/output/workspace && echo some stuff > /workspace/output/workspace/stuff")),
-		tb.Step("write-new-stuff", "ubuntu", tb.StepCommand("bash"), tb.StepArgs("-c", "echo some stuff > /workspace/damnworkspace/stuff")),
+		tb.Step("write-new-stuff", "ubuntu", tb.StepCommand("bash"), tb.StepArgs("-c", "ln -s /workspace/damnworkspace /workspace/output/workspace && echo some stuff > /workspace/output/workspace/stuff")),
+		//tb.Step("write-new-stuff", "ubuntu", tb.StepCommand("bash"), tb.StepArgs("-c", "echo some stuff > /workspace/damnworkspace/stuff")),
 	}
 
 	return tb.Task(taskname, namespace, tb.TaskSpec(taskSpecOps...))
