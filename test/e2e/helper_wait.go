4c8e7d4ddfec795d6d70e2b955b93adced19d30f
praveen4g0 <37117046+praveen4g0@users.noreply.github.com>
2019-10-16 17:52:36 +0530

    Update helper_wait.go

--- test/e2e/helper_wait.go
+++ test/e2e/helper_wait.go
@@ -106,7 +106,57 @@ func WaitForPipelineRunToStart(c *Clients, prname string, namespace string) {
 
 //Wait for Pipeline Run to complete
 func WaitForPipelineRunToComplete(c *Clients, prname string, namespace string) {
-	taskrunList, _ := c.TaskRunClient.List(metav1.ListOptions{LabelSelector: fmt.Sprintf("tekton.dev/pipelineRun=%s", prname)})
+	log.Printf("Waiting for Pipelinerun %s in namespace %s to be started", prname, namespace)
+	if err := WaitForPipelineRunState(c, prname, timeout, func(pr *v1alpha1.PipelineRun) (bool, error) {
+		c := pr.Status.GetCondition(apis.ConditionSucceeded)
+		if c != nil {
+			if c.Status == corev1.ConditionTrue || c.Status == corev1.ConditionFalse {
+				return true, xerrors.Errorf("pipelineRun %s already finished!", prname)
+			} else if c.Status == corev1.ConditionUnknown && (c.Reason == "Running" || c.Reason == "Pending") {
+				return true, nil
+			}
+		}
+		return false, nil
+	}, "PipelineRunRunning"); err != nil {
+		log.Fatalf("Error waiting for PipelineRun %s to be running: %s", prname, err)
+	}
+
+	taskrunList, err := c.TaskRunClient.List(metav1.ListOptions{LabelSelector: fmt.Sprintf("tekton.dev/pipelineRun=%s", prname)})
+
+	if err != nil {
+		log.Fatalf("Error listing TaskRuns for PipelineRun %s: %s", prname, err)
+	}
+
+	log.Printf("Waiting for TaskRuns from PipelineRun %s in namespace %s to be running", prname, namespace)
+	errChan := make(chan error, len(taskrunList.Items))
+	defer close(errChan)
+
+	for _, taskrunItem := range taskrunList.Items {
+		go func(name string) {
+			err := WaitForTaskRunState(c, name, func(tr *v1alpha1.TaskRun) (bool, error) {
+				c := tr.Status.GetCondition(apis.ConditionSucceeded)
+				if c != nil {
+					if c.Status == corev1.ConditionTrue || c.Status == corev1.ConditionFalse {
+						return true, xerrors.Errorf("taskRun %s already finished!", name)
+					} else if c.Status == corev1.ConditionUnknown && (c.Reason == "Running" || c.Reason == "Pending") {
+						return true, nil
+					}
+				}
+				return false, nil
+			}, "TaskRunRunning")
+			errChan <- err
+		}(taskrunItem.Name)
+	}
+
+	for i := 1; i <= len(taskrunList.Items); i++ {
+		if <-errChan != nil {
+			log.Fatalf("Error waiting for TaskRun %s to be running: %s", taskrunList.Items[i-1].Name, err)
+		}
+	}
+
+	if _, err := c.PipelineRunClient.Get(prname, metav1.GetOptions{}); err != nil {
+		log.Fatalf("Failed to get PipelineRun `%s`: %s", prname, err)
+	}
 
 	log.Printf("Waiting for PipelineRun %s in namespace %s to be Completed", prname, namespace)
 	if err := WaitForPipelineRunState(c, prname, timeout, func(pr *v1alpha1.PipelineRun) (bool, error) {
@@ -151,4 +201,3 @@ func WaitForPipelineRunToComplete(c *Clients, prname string, namespace string) {
 		log.Fatalf("Failed to get PipelineRun `%s`: %s", prname, err)
 	}
 }
-
