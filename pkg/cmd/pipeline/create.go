d2034d13f1b222b13b2a3b0247ea17b69d097511
Daniel Helfand <helfand.4@gmail.com>
2019-12-03 00:00:29 -0500

    add pipeline create command

---
+++ pkg/cmd/pipeline/create.go
@@ -0,0 +1,101 @@
+// Copyright © 2019 The Tekton Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pipeline
+
+import (
+	"fmt"
+
+	"github.com/ghodss/yaml"
+
+	"github.com/spf13/cobra"
+	"github.com/tektoncd/cli/pkg/cli"
+	"github.com/tektoncd/cli/pkg/helper/file"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	cliopts "k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+type createOptions struct {
+	from string
+}
+
+func createCommand(p cli.Params) *cobra.Command {
+	f := cliopts.NewPrintFlags("create")
+	opts := &createOptions{from: ""}
+	eg := `
+# Create a Pipeline defined by foo.yaml in namespace 'bar'
+tkn pipeline create -f foo.yaml -n bar
+`
+
+	c := &cobra.Command{
+		Use:          "create",
+		Short:        "Create a pipeline in a namespace",
+		Example:      eg,
+		SilenceUsage: true,
+		Annotations: map[string]string{
+			"commandType": "main",
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			s := &cli.Stream{
+				In:  cmd.InOrStdin(),
+				Out: cmd.OutOrStdout(),
+				Err: cmd.OutOrStderr(),
+			}
+
+			return createPipeline(s, p, opts.from)
+		},
+	}
+	f.AddFlags(c)
+	c.Flags().StringVarP(&opts.from, "from", "f", "", "local or remote filename to use to create the pipeline")
+	return c
+}
+
+func createPipeline(s *cli.Stream, p cli.Params, path string) error {
+	cs, err := p.Clients()
+	if err != nil {
+		return fmt.Errorf("failed to create tekton client")
+	}
+
+	pipeline, err := loadPipeline(p, path)
+	if err != nil {
+		return err
+	}
+
+	_, err = cs.Tekton.TektonV1alpha1().Pipelines(p.Namespace()).Create(pipeline)
+	if err != nil {
+		return fmt.Errorf("failed to create pipeline %q: %s", pipeline.Name, err)
+	}
+
+	fmt.Fprintf(s.Out, "Pipeline created: %s\n", pipeline.Name)
+	return nil
+}
+
+func loadPipeline(p cli.Params, target string) (*v1alpha1.Pipeline, error) {
+	content, err := file.LoadFileContent(p, target, file.IsYamlFile(), fmt.Errorf("inavlid file format for %s: .yaml or .yml file extension and format required", target))
+	if err != nil {
+		return nil, err
+	}
+
+	var pipeline v1alpha1.Pipeline
+	err = yaml.Unmarshal(content, &pipeline)
+	if err != nil {
+		return nil, err
+	}
+
+	if pipeline.Kind != "Pipeline" {
+		return nil, fmt.Errorf("provided kind %s instead of kind Pipeline", pipeline.Kind)
+	}
+
+	return &pipeline, nil
+}
---
+++ pkg/cmd/pipeline/create_test.go
@@ -0,0 +1,117 @@
+// Copyright © 2019 The Tekton Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pipeline
+
+import (
+	"io"
+	"testing"
+
+	"github.com/tektoncd/cli/pkg/test"
+	pipelinetest "github.com/tektoncd/pipeline/test"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_Pipeline_Create(t *testing.T) {
+
+	ns := []*corev1.Namespace{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "ns",
+			},
+		},
+	}
+
+	seeds := make([]pipelinetest.Clients, 0)
+	for i := 0; i < 1; i++ {
+		cs, _ := test.SeedTestData(t, pipelinetest.Data{Namespaces: ns})
+		seeds = append(seeds, cs)
+	}
+
+	testParams := []struct {
+		name        string
+		command     []string
+		input       pipelinetest.Clients
+		inputStream io.Reader
+		wantError   bool
+		want        string
+	}{
+		{
+			name:        "Create pipeline successfully",
+			command:     []string{"create", "--from", "./testdata/pipeline.yaml", "-n", "ns"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   false,
+			want:        "Pipeline created: test-pipeline\n",
+		},
+		{
+			name:        "Filename does not exist",
+			command:     []string{"create", "-f", "./testdata/notexist.yaml", "-n", "ns"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   true,
+			want:        "open ./testdata/notexist.yaml: no such file or directory",
+		},
+		{
+			name:        "Unsupported file type",
+			command:     []string{"create", "-f", "./testdata/pipeline.txt", "-n", "ns"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   true,
+			want:        "inavlid file format for ./testdata/pipeline.txt: .yaml or .yml file extension and format required",
+		},
+		{
+			name:        "Mismatched resource file",
+			command:     []string{"create", "-f", "./testdata/pipelinerun.yaml", "-n", "ns"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   true,
+			want:        "provided kind PipelineRun instead of kind Pipeline",
+		},
+		{
+			name:        "Existing pipeline",
+			command:     []string{"create", "-f", "./testdata/pipeline.yaml", "-n", "ns"},
+			input:       seeds[0],
+			inputStream: nil,
+			wantError:   true,
+			want:        "failed to create pipeline \"test-pipeline\": pipelines.tekton.dev \"test-pipeline\" already exists",
+		},
+	}
+
+	for _, tp := range testParams {
+		t.Run(tp.name, func(t *testing.T) {
+			p := &test.Params{Tekton: tp.input.Pipeline, Kube: tp.input.Kube}
+			pipeline := Command(p)
+
+			if tp.inputStream != nil {
+				pipeline.SetIn(tp.inputStream)
+			}
+
+			out, err := test.ExecuteCommand(pipeline, tp.command...)
+			if tp.wantError {
+				if err == nil {
+					t.Errorf("Error expected here")
+				} else {
+					test.AssertOutput(t, tp.want, err.Error())
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected error")
+				}
+				test.AssertOutput(t, tp.want, out)
+			}
+		})
+	}
+}
