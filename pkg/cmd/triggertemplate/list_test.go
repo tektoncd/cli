57b795176533017aa8803656cbea98b71dbe2fe0
Piyush Garg <piyushgarg001@gmail.com>
2020-01-06 22:20:55 +0530

    Refactor triggertemplate list test

--- pkg/cmd/triggertemplate/list_test.go
+++ pkg/cmd/triggertemplate/list_test.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/jonboulle/clockwork"
+	"github.com/spf13/cobra"
 	"github.com/tektoncd/cli/pkg/test"
 	cb "github.com/tektoncd/cli/pkg/test/builder"
 	"github.com/tektoncd/triggers/pkg/apis/triggers/v1alpha1"
@@ -29,111 +30,102 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestTriggerTemplateList_Invalid_Namespace(t *testing.T) {
-	ns := []*corev1.Namespace{
-		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "default",
-			},
-		},
-	}
-	cs := test.SeedTestResources(t, triggertest.Resources{Namespaces: ns})
-	p := &test.Params{Tekton: cs.Pipeline, Kube: cs.Kube, Triggers: cs.Triggers}
+func TestListTriggerTemplate(t *testing.T) {
+	now := time.Now()
 
-	triggertemplate := Command(p)
-	_, err := test.ExecuteCommand(triggertemplate, "list", "-n", "foo")
-	if err == nil {
-		t.Errorf("Expected error")
-	}
-	test.AssertOutput(t, "namespaces \"foo\" not found", err.Error())
-}
-
-func TestTriggerTemplateList_Empty(t *testing.T) {
 	ns := []*corev1.Namespace{
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "foo",
 			},
 		},
-	}
-	cs := test.SeedTestResources(t, triggertest.Resources{Namespaces: ns})
-	p := &test.Params{Tekton: cs.Pipeline, Kube: cs.Kube, Triggers: cs.Triggers}
-
-	triggertemplate := Command(p)
-	output, err := test.ExecuteCommand(triggertemplate, "list", "-n", "foo")
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-	}
-	test.AssertOutput(t, "No triggertemplates found\n", output)
-}
-
-func TestTriggerTemplateList_Multiple(t *testing.T) {
-	clock := clockwork.NewFakeClock()
-	ns := []*corev1.Namespace{
 		{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "foo",
+				Name: "random",
 			},
 		},
 	}
-	tt := []*v1alpha1.TriggerTemplate{
-		tb.TriggerTemplate("tt1", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-2*time.Minute))),
-		tb.TriggerTemplate("tt2", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-30*time.Second))),
-		tb.TriggerTemplate("tt3", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-200*time.Hour))),
+
+	tts := []*v1alpha1.TriggerTemplate{
+		tb.TriggerTemplate("tt1", "foo", cb.TriggerTemplateCreationTime(now.Add(-2*time.Minute))),
+		tb.TriggerTemplate("tt2", "foo", cb.TriggerTemplateCreationTime(now.Add(-30*time.Second))),
+		tb.TriggerTemplate("tt3", "foo", cb.TriggerTemplateCreationTime(now.Add(-200*time.Hour))),
 		tb.TriggerTemplate("tt4", "foo"),
 	}
-	cs := test.SeedTestResources(t, triggertest.Resources{Namespaces: ns, TriggerTemplates: tt})
-	p := &test.Params{Tekton: cs.Pipeline, Kube: cs.Kube, Triggers: cs.Triggers, Clock: clock}
 
-	triggertemplate := Command(p)
-	output, err := test.ExecuteCommand(triggertemplate, "list", "-n", "foo")
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-	}
-
-	expected := []string{
-		"NAME   AGE",
-		"tt1    2 minutes ago",
-		"tt2    30 seconds ago",
-		"tt3    1 week ago",
-		"tt4    ---",
-		"",
-	}
-	text := strings.Join(expected, "\n")
-	test.AssertOutput(t, text, output)
-}
-
-func TestTriggerTemplateList_Output(t *testing.T) {
-	clock := clockwork.NewFakeClock()
-	ns := []*corev1.Namespace{
+	tests := []struct {
+		name      string
+		command   *cobra.Command
+		args      []string
+		expected  []string
+		wantError bool
+	}{
 		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "foo",
+			name:    "Invalid namespace",
+			command: command(t, tts, now, ns),
+			args:    []string{"list", "-n", "default"},
+			expected: []string{
+				"Error: namespaces \"default\" not found\n",
 			},
+			wantError: true,
+		},
+		{
+			name:    "No TriggerTemplate",
+			command: command(t, tts, now, ns),
+			args:    []string{"list", "-n", "random"},
+			expected: []string{
+				"No triggertemplates found\n",
+			},
+			wantError: false,
+		},
+		{
+			name:    "Multiple TriggerTemplates",
+			command: command(t, tts, now, ns),
+			args:    []string{"list", "-n", "foo"},
+			expected: []string{
+				"NAME   AGE",
+				"tt1    2 minutes ago",
+				"tt2    30 seconds ago",
+				"tt3    1 week ago",
+				"tt4    ---",
+				"",
+			},
+			wantError: false,
+		},
+		{
+			name:    "Multiple TriggerTemplates with output format",
+			command: command(t, tts, now, ns),
+			args:    []string{"list", "-n", "foo", "-o", "jsonpath={range .items[*]}{.metadata.name}{\"\\n\"}{end}"},
+			expected: []string{
+				"tt1",
+				"tt2",
+				"tt3",
+				"tt4",
+				"",
+			},
+			wantError: false,
 		},
 	}
-	tt := []*v1alpha1.TriggerTemplate{
-		tb.TriggerTemplate("tt1", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-2*time.Minute))),
-		tb.TriggerTemplate("tt2", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-30*time.Second))),
-		tb.TriggerTemplate("tt3", "foo", cb.TriggerTemplateCreationTime(clock.Now().Add(-200*time.Hour))),
-		tb.TriggerTemplate("tt4", "foo"),
-	}
-	cs := test.SeedTestResources(t, triggertest.Resources{Namespaces: ns, TriggerTemplates: tt})
-	p := &test.Params{Tekton: cs.Pipeline, Kube: cs.Kube, Triggers: cs.Triggers, Clock: clock}
 
-	triggertemplate := Command(p)
-	output, err := test.ExecuteCommand(triggertemplate, "list", "-n", "foo", "-o", "jsonpath={range .items[*]}{.metadata.name}{\"\\n\"}{end}")
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-	}
+	for _, td := range tests {
+		t.Run(td.name, func(t *testing.T) {
+			got, err := test.ExecuteCommand(td.command, td.args...)
 
-	expected := []string{
-		"tt1",
-		"tt2",
-		"tt3",
-		"tt4",
-		"",
+			if err != nil && !td.wantError {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			test.AssertOutput(t, strings.Join(td.expected, "\n"), got)
+		})
 	}
-	text := strings.Join(expected, "\n")
-	test.AssertOutput(t, text, output)
+}
+
+func command(t *testing.T, tts []*v1alpha1.TriggerTemplate, now time.Time, ns []*corev1.Namespace) *cobra.Command {
+	// fake clock advanced by 1 hour
+	clock := clockwork.NewFakeClockAt(now)
+
+	cs := test.SeedTestResources(t, triggertest.Resources{TriggerTemplates: tts, Namespaces: ns})
+
+	p := &test.Params{Tekton: cs.Pipeline, Clock: clock, Kube: cs.Kube, Triggers: cs.Triggers}
+
+	return Command(p)
 }
