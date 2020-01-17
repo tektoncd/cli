76605b67f66f54ffeb3ef37b6aced3489489a593
Chmouel Boudjnah <chmouel@redhat.com>
2020-01-17 10:46:30 +0100

    Add tests for k8.Condition
    Figured while refactoring this file that this part of the code wasn't tested, so
    let's add some tests..
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/formatted/k8s_test.go
+++ pkg/formatted/k8s_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"knative.dev/pkg/apis/duck/v1beta1"
+
+	corev1 "k8s.io/api/core/v1"
+	"knative.dev/pkg/apis"
 
 	"github.com/fatih/color"
 )
@@ -25,4 +29,85 @@ func TestAutoStepName(t *testing.T) {
 
 	thirdRound := AutoStepName("")
 	assert.Equal(t, thirdRound, "unnamed-2")
+}
+
+func TestCondition(t *testing.T) {
+	tests := []struct {
+		name      string
+		condition v1beta1.Conditions
+		want      string
+	}{
+		{
+			name: "Failed status",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionFalse,
+			}},
+			want: "Failed",
+		},
+		{
+			name: "Succeeded status",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionTrue,
+			}},
+			want: "Succeeded",
+		},
+		{
+			name: "Running status",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionReady,
+				Status: corev1.ConditionUnknown,
+			}},
+			want: "Running",
+		},
+		{
+			name: "PipelineRunCanceled status reason",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionTrue,
+				Reason: "PipelineRunCancelled",
+			}},
+			want: "Cancelled(PipelineRunCancelled)",
+		},
+		{
+			name: "TaskRunCanceled status reason",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionTrue,
+				Reason: "TaskRunCancelled",
+			}},
+			want: "Cancelled(TaskRunCancelled)",
+		},
+		{
+			name: "Reason equal Status",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionTrue,
+				Reason: "Succeeded",
+			}},
+			want: "Succeeded",
+		},
+		{
+			name: "Reason not equal Status",
+			condition: []apis.Condition{{
+				Type:   apis.ConditionSucceeded,
+				Status: corev1.ConditionTrue,
+				Reason: "Ragnarok",
+			}},
+			want: "Succeeded(Ragnarok)",
+		},
+		{
+			name:      "No Conditions",
+			condition: []apis.Condition{},
+			want:      "---",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Condition(tt.condition); got != tt.want {
+				t.Errorf("Condition() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
