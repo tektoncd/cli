a2ecc03b8470b4755aaf197fe9f0ff14e76efe02
Daniel Helfand <helfand.4@gmail.com>
2020-02-10 09:40:29 -0500

    --all option for tkn resource delete

--- pkg/cmd/pipelineresource/delete.go
+++ pkg/cmd/pipelineresource/delete.go
@@ -27,7 +27,7 @@ import (
 )
 
 func deleteCommand(p cli.Params) *cobra.Command {
-	opts := &options.DeleteOptions{Resource: "pipelineresource", ForceDelete: false}
+	opts := &options.DeleteOptions{Resource: "pipelineresource", ForceDelete: false, DeleteAllNs: false}
 	f := cliopts.NewPrintFlags("delete")
 	eg := `Delete PipelineResources with names 'foo' and 'bar' in namespace 'quux':
 
@@ -43,7 +43,7 @@ or
 		Aliases:      []string{"rm"},
 		Short:        "Delete pipeline resources in a namespace",
 		Example:      eg,
-		Args:         cobra.MinimumNArgs(1),
+		Args:         cobra.MinimumNArgs(0),
 		SilenceUsage: true,
 		Annotations: map[string]string{
 			"commandType": "main",
@@ -63,17 +63,18 @@ or
 				return err
 			}
 
-			return deleteResources(s, p, args)
+			return deleteResources(s, p, args, opts.DeleteAllNs)
 		},
 	}
 	f.AddFlags(c)
 	c.Flags().BoolVarP(&opts.ForceDelete, "force", "f", false, "Whether to force deletion (default: false)")
+	c.Flags().BoolVarP(&opts.DeleteAllNs, "all", "", false, "Delete all PipelineResources in a namespace (default: false)")
 
 	_ = c.MarkZshCompPositionalArgumentCustom(1, "__tkn_get_pipelineresource")
 	return c
 }
 
-func deleteResources(s *cli.Stream, p cli.Params, preNames []string) error {
+func deleteResources(s *cli.Stream, p cli.Params, preNames []string, deleteAll bool) error {
 	cs, err := p.Clients()
 	if err != nil {
 		return fmt.Errorf("failed to create tekton client")
@@ -82,7 +83,32 @@ func deleteResources(s *cli.Stream, p cli.Params, preNames []string) error {
 	d := deleter.New("PipelineResource", func(resourceName string) error {
 		return cs.Resource.TektonV1alpha1().PipelineResources(p.Namespace()).Delete(resourceName, &metav1.DeleteOptions{})
 	})
+	if deleteAll {
+		preNames, err = allPipelineResourceNames(p, cs)
+		if err != nil {
+			return err
+		}
+	}
 	d.Delete(s, preNames)
-	d.PrintSuccesses(s)
+
+	if !deleteAll {
+		d.PrintSuccesses(s)
+	} else if deleteAll {
+		if d.Errors() == nil {
+			fmt.Fprintf(s.Out, "All PipelineResources deleted in namespace %q\n", p.Namespace())
+		}
+	}
 	return d.Errors()
+}
+
+func allPipelineResourceNames(p cli.Params, cs *cli.Clients) ([]string, error) {
+	resources, err := cs.Resource.TektonV1alpha1().PipelineResources(p.Namespace()).List(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var names []string
+	for _, resource := range resources.Items {
+		names = append(names, resource.Name)
+	}
+	return names, nil
 }
