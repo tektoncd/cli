56653bf08c87fad66396d3b01bdcf9c1b01567b3
Vincent Demeester <vdemeest@redhat.com>
2020-02-28 11:25:45 +0100

    Add support for yubikey in debbuild 🦌
    If the release captain passes `YUBIKEY` environment variable with the
    `/dev/…` as value, it will add the correct flag to let the container
    being able to see and use it.
    
    Example: `YUBIKEY=/dev/hidraw11 ./run.sh`
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- tekton/debbuild/run.sh
+++ tekton/debbuild/run.sh
@@ -7,12 +7,21 @@ set -eux
 # and have your gpg setup in your profile
 PPATARGET=tektoncd/cli
 GPG_KEY=${GPG_KEY}
+YUBIKEY=${YUBIKEY:-}
+ADDITIONAL_ARGS=${ADDITIONAL_ARGS:-}
 
 [[ -n ${GPG_KEY} ]] || {
-	echo "You need to setup your GPG_KEY"
-	exit 1
+    echo "You need to setup your GPG_KEY"
+    exit 1
 }
 
+[[ -n ${YUBIKEY} ]] && {
+    ADDITIONAL_ARGS="${ADDITIONAL_ARGS} -v /dev/bus/usb:/dev/bus/usb \
+     -v /sys/bus/usb:/sys/bus/usb \
+     -v /sys/devices:/sys/devices \
+     -v ${YUBIKEY}:${YUBIKEY} \
+     --privileged"
+}
 
 gpg --list-secret-keys >/dev/null || { echo "You need to have a secret GPG key"; exit 1 ;}
 
@@ -22,9 +31,10 @@ docker build -t ubuntu-build-deb .
 cd ..
 fpath=$(readlink -f control)
 docker run --rm \
-	 -v ~/.gnupg:/root/.gnupg \
-	 -v ${fpath}:/debian --name ubuntu-build-deb \
-	 --env PPATARGET=${PPATARGET} \
-	 --env GPG_KEY=${GPG_KEY} \
-	 -it ubuntu-build-deb \
-	/run.sh
+       -v ~/.gnupg:/root/.gnupg \
+       $ADDITIONAL_ARGS \
+       -v ${fpath}:/debian --name ubuntu-build-deb \
+       --env PPATARGET=${PPATARGET} \
+       --env GPG_KEY=${GPG_KEY} \
+       -it ubuntu-build-deb \
+       /run.sh
