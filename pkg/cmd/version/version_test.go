471f3ba015cf9987f367b647169753552029815f
Divyansh42 <diagrawa@redhat.com>
2020-08-26 08:59:46 +0530

    Modify tkn version output to hide Triggers and Dashboard version if they are not installed and added required unit tests.
    Signed-off-by: Divyansh42 <diagrawa@redhat.com>

--- pkg/cmd/version/version_test.go
+++ pkg/cmd/version/version_test.go
@@ -22,6 +22,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -29,6 +30,9 @@ import (
 	pipelinetest "github.com/tektoncd/pipeline/test/v1alpha1"
 	"gotest.tools/v3/assert"
 	"gotest.tools/v3/golden"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestVersionGood(t *testing.T) {
@@ -149,4 +153,105 @@ func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
 	}
 
 	return cli, s.Close
+}
+
+func TestGetVersions(t *testing.T) {
+	pipelineDeploymentLabels := map[string]string{
+		"app.kubernetes.io/part-of":   "tekton-pipelines",
+		"app.kubernetes.io/component": "controller",
+		"app.kubernetes.io/name":      "controller",
+	}
+	triggersDeploymentLabels := map[string]string{
+		"app.kubernetes.io/part-of":   "tekton-triggers",
+		"app.kubernetes.io/component": "controller",
+		"app.kubernetes.io/name":      "controller",
+	}
+	dashboardDeploymentLabels := map[string]string{
+		"app.kubernetes.io/part-of":   "tekton-dashboard",
+		"app.kubernetes.io/component": "dashboard",
+		"app.kubernetes.io/name":      "dashboard",
+	}
+	pipelineDeployment := getDeploymentData("pipeline-dep", "", pipelineDeploymentLabels, map[string]string{"app.kubernetes.io/version": "v0.10.0"}, nil)
+	triggersDeployment := getDeploymentData("triggers-dep", "", triggersDeploymentLabels, map[string]string{"app.kubernetes.io/version": "v0.5.0"}, nil)
+	dashboardDeployment := getDeploymentData("dashboard-dep", "", dashboardDeploymentLabels, map[string]string{"app.kubernetes.io/version": "v0.7.0"}, nil)
+
+	testParams := []struct {
+		name                  string
+		namespace             string
+		userProvidedNamespace string
+		deployment            []*v1.Deployment
+		goldenFile            bool
+	}{{
+		name:       "empty deployment items",
+		namespace:  "tekton-pipelines",
+		deployment: []*v1.Deployment{},
+		goldenFile: true,
+	}, {
+		name:                  "deployment only with pipeline installed",
+		namespace:             "test",
+		userProvidedNamespace: "test",
+		deployment:            []*v1.Deployment{pipelineDeployment},
+		goldenFile:            true,
+	}, {
+		name:                  "deployment with pipeline and triggers installed",
+		namespace:             "test",
+		userProvidedNamespace: "test",
+		deployment:            []*v1.Deployment{pipelineDeployment, triggersDeployment},
+		goldenFile:            true,
+	}, {
+		name:                  "deployment with pipeline and dashboard installed",
+		namespace:             "test",
+		userProvidedNamespace: "test",
+		deployment:            []*v1.Deployment{pipelineDeployment, dashboardDeployment},
+		goldenFile:            true,
+	}, {
+		name:                  "deployment with pipeline, triggers and dashboard installed",
+		namespace:             "test",
+		userProvidedNamespace: "test",
+		deployment:            []*v1.Deployment{pipelineDeployment, triggersDeployment, dashboardDeployment},
+		goldenFile:            true,
+	}}
+	for _, tp := range testParams {
+		t.Run(tp.name, func(t *testing.T) {
+			seedData, _ := test.SeedTestData(t, pipelinetest.Data{})
+			cs := pipelinetest.Clients{Kube: seedData.Kube}
+			p := &test.Params{Kube: cs.Kube}
+			version := Command(p)
+			cls, err := p.Clients()
+			if err != nil {
+				t.Errorf("failed to get client: %v", err)
+			}
+			// To add multiple deployments in a particular namespace
+			for _, v := range tp.deployment {
+				if _, err := cls.Kube.AppsV1().Deployments(tp.namespace).Create(v); err != nil {
+					t.Errorf("failed to create deployment: %v", err)
+				}
+			}
+			got, _ := test.ExecuteCommand(version, "version", "-n", "test")
+			golden.Assert(t, got, strings.ReplaceAll(fmt.Sprintf("%s.golden", t.Name()), "/", "-"))
+		})
+	}
+}
+
+func getDeploymentData(name, image string, deploymentLabels, podTemplateLabels, annotations map[string]string) *v1.Deployment {
+	return &v1.Deployment{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: deploymentLabels,
+		},
+		Spec: v1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels:      podTemplateLabels,
+					Annotations: annotations,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: image,
+					}},
+				},
+			},
+		},
+	}
 }
