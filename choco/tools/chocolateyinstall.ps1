e88a75ae37cf679a1ea6601c39cecd446b2b9616
Piyush Garg <pgarg@redhat.com>
2022-03-25 21:03:05 +0530

    Bump choco spec to v0.23.1

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.23.0/tkn_0.23.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.23.1/tkn_0.23.1_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '377c228c3c3266dbfec0081fda2216760f6059eb7c40469ce7ac873d4795fab0'
+  checksum64     = '7f8ffdf830e52b856e6b7e389bd62bad9e30e1c4e6e909bd80034a58a021ca13'
   checksumType64 = 'sha256'
 }
 
