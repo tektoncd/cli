1765e51eafb9f262e7c3b624d83097b5a8731426
Daniel Helfand <helfand.4@gmail.com>
2022-06-13 09:01:48 -0400

    update chocolatey package to 0.24.0

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.23.1/tkn_0.23.1_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.24.0/tkn_0.24.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '7f8ffdf830e52b856e6b7e389bd62bad9e30e1c4e6e909bd80034a58a021ca13'
+  checksum64     = '618683DC0E226373814560ACB260FA31A806648825AA184F623AF086E74FD3E9'
   checksumType64 = 'sha256'
 }
 
