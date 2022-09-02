6c0e8ed230e5d405d7f59f77a5407317a0fd1eba
Piyush Garg <pgarg@redhat.com>
2022-09-02 21:49:29 +0530

    Update docs to v0.26.0
    This will update readme and chocolaty pkg docs
    to v0.26.0

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.25.0/tkn_0.25.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.26.0/tkn_0.26.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '29b98eceacd86f67aa10436907075af0fd18b3f370746b9af18e62d734b12786'
+  checksum64     = 'a0b42c1300175ce379dadb822ca3633c34048eae0064533d7718746c79f11a65'
   checksumType64 = 'sha256'
 }
 
