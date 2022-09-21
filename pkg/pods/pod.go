601270d7df18df0e15d361d7dc0356fdffdf2f45
Pradeep Kumar <pradkuma@redhat.com>
2022-09-21 17:20:58 +0530

    fix "send on closed channel"
    this fixes panic on send on closed channel with logs commands in
    follow mode.
    before writing to the event channel we check if we still need to
    update.
    Ones we get some update on the pod we ask the SharedInformer to
    stop by sending signal to stopC
    
    Only concern is not able to test it with actual panic scenario.
    
    Signed-off-by: Pradeep Kumar <pradkuma@redhat.com>

--- pkg/pods/pod.go
+++ pkg/pods/pod.go
@@ -69,18 +69,14 @@ func NewWithDefaults(name, ns string, client k8s.Interface) *Pod {
 	}
 }
 
-// Wait wait for the pod to get up and running
+// Wait waits for the pod to get up and running
 func (p *Pod) Wait() (*corev1.Pod, error) {
-	// ensure pod exists before we actually check for it
 	if _, err := p.Get(); err != nil {
 		return nil, err
 	}
 
 	stopC := make(chan struct{})
 	eventC := make(chan interface{})
-	defer close(eventC)
-	defer close(stopC)
-
 	p.watcher(stopC, eventC)
 
 	var pod *corev1.Pod
@@ -88,10 +84,10 @@ func (p *Pod) Wait() (*corev1.Pod, error) {
 	for e := range eventC {
 		pod, err = checkPodStatus(e)
 		if pod != nil || err != nil {
-			break
+			close(stopC)
+			return pod, err
 		}
 	}
-
 	return pod, err
 }
 
@@ -103,9 +99,32 @@ func (p *Pod) watcher(stopC <-chan struct{}, eventC chan<- interface{}) {
 
 	factory.Core().V1().Pods().Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    func(obj interface{}) { eventC <- obj },
-			UpdateFunc: func(oldObj, newObj interface{}) { eventC <- newObj },
-			DeleteFunc: func(obj interface{}) { eventC <- obj },
+			// first check if update still needed.
+			AddFunc: func(obj interface{}) {
+
+				stopCh := stopC
+				select {
+				case <-stopCh:
+					return
+				case eventC <- obj:
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				stopCh := stopC
+				select {
+				case <-stopCh:
+					return
+				case eventC <- newObj:
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				stopCh := stopC
+				select {
+				case <-stopCh:
+					return
+				case eventC <- obj:
+				}
+			},
 		})
 
 	factory.Start(stopC)
