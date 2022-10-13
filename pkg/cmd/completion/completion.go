f0f351e65536f49d09c29abe953f3b3db2e440cd
Piyush Garg <pgarg@redhat.com>
2022-10-13 12:17:58 +0530

    Get rid of deprecated cobra functions

--- pkg/cmd/completion/completion.go
+++ pkg/cmd/completion/completion.go
@@ -83,7 +83,7 @@ func Command() *cobra.Command {
 		Annotations: map[string]string{
 			"commandType": "utility",
 		},
-		Args: cobra.ExactValidArgs(1),
+		Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			switch args[0] {
 			case "bash":
