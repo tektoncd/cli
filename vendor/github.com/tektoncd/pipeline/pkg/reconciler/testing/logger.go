3c6fd52750665c0ade97c9e9ac139dae9a687447
Piyush Garg <pgarg@redhat.com>
2023-02-01 23:50:19 +0530

    Bump tektoncd/pipeline to v0.44.0

--- vendor/github.com/tektoncd/pipeline/pkg/reconciler/testing/logger.go
+++ vendor/github.com/tektoncd/pipeline/pkg/reconciler/testing/logger.go
@@ -23,6 +23,7 @@ import (
 
 // SetupFakeContext sets up the Context and the fake filtered informers for the tests.
 func SetupFakeContext(t *testing.T) (context.Context, []controller.Informer) {
+	t.Helper()
 	ctx, _, informer := setupFakeContextWithLabelKey(t)
 	return WithLogger(ctx, t), informer
 }
@@ -37,16 +38,18 @@ func SetupFakeCloudClientContext(ctx context.Context, expectedEventCount int) co
 
 // SetupDefaultContext sets up the Context and the default filtered informers for the tests.
 func SetupDefaultContext(t *testing.T) (context.Context, []controller.Informer) {
+	t.Helper()
 	ctx, _, informer := setupDefaultContextWithLabelKey(t)
 	return WithLogger(ctx, t), informer
 }
 
-// WithLogger returns the the Logger
+// WithLogger returns the Logger
 func WithLogger(ctx context.Context, t *testing.T) context.Context {
+	t.Helper()
 	return logging.WithLogger(ctx, TestLogger(t))
 }
 
-// TestLogger sets up the the Logger
+// TestLogger sets up the Logger
 func TestLogger(t *testing.T) *zap.SugaredLogger {
 	logger := zaptest.NewLogger(t, zaptest.WrapOptions(zap.AddCaller()))
 	return logger.Sugar().Named(t.Name())
