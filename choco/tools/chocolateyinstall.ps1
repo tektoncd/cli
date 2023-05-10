286a4664b312e73ff839e592039c4f78547d2d68
vinamra28 <jvinamra776@gmail.com>
2023-05-10 13:33:57 +0530

    Bump tkn version in README to latest
    Bumps the version of tkn v0.31.0 in README.md, choco/ and releases.md
    
    Signed-off-by: vinamra28 <jvinamra776@gmail.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.30.1/tkn_0.30.1_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.31.0/tkn_0.31.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'bafb35a3ed707a4d7e02422fa4573ee85f5d9710a323feec553fd4f4cc7cdd45'
+  checksum64     = '6d22ad060905e4b3ab6316fe1463b70a33dc3b94b264a332c27dc0a13a4c394b'
   checksumType64 = 'sha256'
 }
 
