5f30d85db94f13c39c70f44e077e6b20acb46f3c
Piyush Garg <pgarg@redhat.com>
2023-06-14 00:29:57 +0530

    Bump tkn to v0.31.1 in docs

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.31.0/tkn_0.31.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.31.1/tkn_0.31.1_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '6d22ad060905e4b3ab6316fe1463b70a33dc3b94b264a332c27dc0a13a4c394b'
+  checksum64     = 'f9a8c39d0ee6cf03a2a092466b0578c8740a4bddfc1910ce3042868cfac891b1'
   checksumType64 = 'sha256'
 }
 
