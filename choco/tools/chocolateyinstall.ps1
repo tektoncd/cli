024b2314b17f594e0de87856bc5d2a5ff2a394ef
Piyush Garg <pgarg@redhat.com>
2023-10-19 09:58:19 +0530

    Update docs for v0.32.1

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.32.0/tkn_0.32.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.32.1/tkn_0.32.1_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '708342c75e9b1ddf957b0f5bb4474f10051b998459cfd17b2a08bf18c734c881'
+  checksum64     = '14dd923656ea9426b71e50d4f0856b03c341ee3f7453082061129bec1d6b7161'
   checksumType64 = 'sha256'
 }
 
