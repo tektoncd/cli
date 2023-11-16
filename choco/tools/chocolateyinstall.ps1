f73ca11058b3bd48a7fd3b1fe6eea2046f2996d5
vinamra28 <jvinamra776@gmail.com>
2023-11-16 12:05:26 +0530

    Bump choco and readme to v0.33.0
    Signed-off-by: vinamra28 <jvinamra776@gmail.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.32.2/tkn_0.32.2_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.33.0/tkn_0.33.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'b179199a2a2c7a551e2726f3c65be8df928b945701bc1d7d2b837a718bbdbdd6'
+  checksum64     = '85efa3c755c235b381e1591d26911d163b39f165879dcf5a4f6f4d9f18d7ddef'
   checksumType64 = 'sha256'
 }
 
