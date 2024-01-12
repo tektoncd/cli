b6c023f2e2ad77d63e4ce86d57fabba39788d974
vinamra28 <jvinamra776@gmail.com>
2024-01-12 11:48:06 +0530

    Bump tkn to latest version v0.34.0
    - Update the readme
    - Update the choco directory to point to 0.34.0

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.33.0/tkn_0.33.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.34.0/tkn_0.34.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '85efa3c755c235b381e1591d26911d163b39f165879dcf5a4f6f4d9f18d7ddef'
+  checksum64     = 'b87ceebeea935522faab40d420d5893d52de01437fb386eeab9556167e6b20fe'
   checksumType64 = 'sha256'
 }
 
