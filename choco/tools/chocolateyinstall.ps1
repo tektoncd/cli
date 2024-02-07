2c3e315eeaeef4cfe544edc7b2f364840b6e46f4
Piyush Garg <pgarg@redhat.com>
2024-02-08 01:25:15 +0530

    Bump docs to latest v0.35.0
    This will bump docs of tkn to latest version v0.35.0
    and also update choco file

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.34.0/tkn_0.34.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.35.0/tkn_0.35.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'b87ceebeea935522faab40d420d5893d52de01437fb386eeab9556167e6b20fe'
+  checksum64     = 'dc8936b285715bbdd8eb9335ce674d852cf1b2cd959335ca6438f6faa8dfd539'
   checksumType64 = 'sha256'
 }
 
