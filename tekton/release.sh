a968cae760ddc83819b530e317fe767b2440a65b
Piyush Garg <pgarg@redhat.com>
2024-09-03 22:08:29 +0530

    Bump tektoncd/pipeline to v0.62.3
    This will bump tektoncd/pipeline to v0.62.3 to make
    client compatible with old pipeline versions
    
    Fix #2388

--- tekton/release.sh
+++ tekton/release.sh
@@ -14,10 +14,12 @@ BINARIES="kubectl jq tkn git"
 
 set -e
 
-for b in ${BINARIES};do
-    type -p ${b} >/dev/null || { echo "'${b}' need to be avail"; exit 1 ;}
+for b in ${BINARIES}; do
+    type -p ${b} >/dev/null || {
+        echo "'${b}' need to be avail"
+        exit 1
+    }
 done
-
 
 kubectl version 2>/dev/null >/dev/null || {
     echo "you need to have access to a kubernetes cluster"
@@ -31,16 +33,23 @@ kubectl get pipeline 2>/dev/null >/dev/null || {
 
 [[ -z ${RELEASE_VERSION} ]] && {
     read -e -p "Enter a target release (i.e: v0.1.2): " RELEASE_VERSION
-    [[ -z ${RELEASE_VERSION} ]] && { echo "no target release"; exit 1 ;}
+    [[ -z ${RELEASE_VERSION} ]] && {
+        echo "no target release"
+        exit 1
+    }
 }
 
-[[ ${RELEASE_VERSION} =~ v[0-9]+\.[0-9]*\.[0-9]+ ]] || { echo "invalid version provided, need to match v\d+\.\d+\.\d+"; exit 1 ;}
+[[ ${RELEASE_VERSION} =~ v[0-9]+\.[0-9]*\.[0-9]+ ]] || {
+    echo "invalid version provided, need to match v\d+\.\d+\.\d+"
+    exit 1
+}
 git fetch -a --tags ${UPSTREAM_REMOTE} >/dev/null
-lasttag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo ${lasttag}|sed 's/\.[0-9]*$//'|grep -q ${RELEASE_VERSION%.*} && {
-    echo "Minor version of ${RELEASE_VERSION%.*} detected, previous ${lasttag}"; minor_version=true ;
+lasttag=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo ${lasttag} | sed 's/\.[0-9]*$//' | grep -q ${RELEASE_VERSION%.*} && {
+    echo "Minor version of ${RELEASE_VERSION%.*} detected, previous ${lasttag}"
+    minor_version=true
 } || {
-    echo "Major version for ${RELEASE_VERSION%.*} detected, previous ${lasttag}";
+    echo "Major version for ${RELEASE_VERSION%.*} detected, previous ${lasttag}"
 }
 
 cd ${GOPATH}/src/github.com/tektoncd/cli
@@ -53,25 +62,31 @@ cd ${GOPATH}/src/github.com/tektoncd/cli
 
 git checkout ${DEFAULT_BRANCH}
 git reset --hard ${UPSTREAM_REMOTE}/${DEFAULT_BRANCH}
-git checkout -B release-${RELEASE_VERSION}  ${DEFAULT_BRANCH} >/dev/null
+git checkout -B release-${RELEASE_VERSION} ${DEFAULT_BRANCH} >/dev/null
 
-if [[ -n ${minor_version} ]];then
+if [[ -n ${minor_version} ]]; then
     git reset --hard ${lasttag} >/dev/null
 
-    if [[ -z ${COMMITS} ]];then
+    if [[ -z ${COMMITS} ]]; then
         echo "Showing commit between last minor tag '${lasttag} to '${DEFAULT_BRANCH}'"
         echo
         git log --reverse --no-merges --pretty=format:"%C(bold cyan)%h%Creset | %cd | %s | %ae" ${DEFAULT_BRANCH} --since "$(git log --pretty=format:%cd -1 ${lasttag})"
         echo
         read -e -p "Pick a list of ordered commits to cherry-pick space separated (* mean all of them): " COMMITS
     fi
-    [[ -z ${COMMITS} ]] && { echo "no commits picked"; exit 1;}
-    if [[ ${COMMITS} == "*" ]];then
+    [[ -z ${COMMITS} ]] && {
+        echo "no commits picked"
+        exit 1
+    }
+    if [[ ${COMMITS} == "*" ]]; then
         COMMITS=$(git log --reverse --no-merges --pretty=format:"%h" ${DEFAULT_BRANCH} \
-                      --since "$(git log --pretty=format:%cd -1 ${lasttag})")
+            --since "$(git log --pretty=format:%cd -1 ${lasttag})")
     fi
-    for commit in ${COMMITS};do
-        git branch --contains ${commit} >/dev/null || { echo "Invalid commit ${commit}" ; exit 1;}
+    for commit in ${COMMITS}; do
+        git branch --contains ${commit} >/dev/null || {
+            echo "Invalid commit ${commit}"
+            exit 1
+        }
         echo "Cherry-picking commit: ${commit}"
         git cherry-pick ${commit} >/dev/null
     done
@@ -89,19 +104,19 @@ fi
 }
 
 COMMITS=$(git log --reverse --no-merges \
-              --pretty=format:'%H' ${DEFAULT_BRANCH} \
-              --since "$(git log --pretty=format:%cd -1 ${lasttag})")
+    --pretty=format:'%H' ${DEFAULT_BRANCH} \
+    --since "$(git log --pretty=format:%cd -1 ${lasttag})")
 
 changelog=""
-for c in ${COMMITS};do
-    pr=$(curl -s "https://api.github.com/search/issues?q=${c}" |jq -r '.items[0].html_url')
-    pr=$(echo ${pr}|sed 's,https://github.com/tektoncd/cli/pull/,tektoncd/cli#,')
+for c in ${COMMITS}; do
+    pr=$(curl -s "https://api.github.com/search/issues?q=${c}" | jq -r '.items[0].html_url')
+    pr=$(echo ${pr} | sed 's,https://github.com/tektoncd/cli/pull/,tektoncd/cli#,')
     changelog+="${pr} | $(git log -1 --date=format:'%Y/%m/%d-%H:%M' --pretty=format:'[%an] %s | %cd' ${c})
 "
 done
 
 # Add our VERSION so Makefile will pick it up when compiling
-echo ${RELEASE_VERSION#v} > VERSION
+echo ${RELEASE_VERSION#v} >VERSION
 git add VERSION
 git commit -sm "New version ${RELEASE_VERSION}" -m "${changelog}" VERSION
 git tag --sign -m \
@@ -113,12 +128,12 @@ git push --force ${PUSH_REMOTE} release-${RELEASE_VERSION}
 
 kubectl create namespace ${TARGET_NAMESPACE} 2>/dev/null || true
 
-for task in ${CATALOG_TASKS};do
-  if [ ${task} == "lint" ]; then
-    tkn -n ${TARGET_NAMESPACE} hub install task golangci-lint
-  else
-    tkn -n ${TARGET_NAMESPACE} hub install task golang-${task}
-  fi
+for task in ${CATALOG_TASKS}; do
+    if [ ${task} == "lint" ]; then
+        tkn -n ${TARGET_NAMESPACE} hub install task golangci-lint
+    else
+        tkn -n ${TARGET_NAMESPACE} hub install task golang-${task}
+    fi
 done
 
 tkn -n ${TARGET_NAMESPACE} hub install task git-clone
@@ -127,12 +142,15 @@ tkn -n ${TARGET_NAMESPACE} hub install task goreleaser
 kubectl -n ${TARGET_NAMESPACE} apply -f ./tekton/get-version.yaml
 kubectl -n ${TARGET_NAMESPACE} apply -f ./tekton/publish.yaml
 
-if ! kubectl -n ${TARGET_NAMESPACE} get secret ${SECRET_NAME} -o name >/dev/null 2>/dev/null;then
+if ! kubectl -n ${TARGET_NAMESPACE} get secret ${SECRET_NAME} -o name >/dev/null 2>/dev/null; then
     github_token=$(git config --get github.oauth-token || true)
 
     [[ -z ${github_token} ]] && {
         read -e -p "Enter your Github Token: " github_token
-        [[ -z ${github_token} ]] && { echo "no token provided"; exit 1;}
+        [[ -z ${github_token} ]] && {
+            echo "no token provided"
+            exit 1
+        }
     }
 
     kubectl -n ${TARGET_NAMESPACE} create secret generic ${SECRET_NAME} --from-literal=bot-token=${github_token}
@@ -145,8 +163,8 @@ sleep 2
 URL=$(git remote get-url "${PUSH_REMOTE}" | sed 's,git@github.com:,https://github.com/,')
 # Start the pipeline
 tkn -n ${TARGET_NAMESPACE} pipeline start cli-release-pipeline \
-  -p revision="${RELEASE_VERSION}" \
-  -p url="${URL}" \
-  -w name=shared-workspace,volumeClaimTemplateFile=./tekton/volume-claim-template.yml
+    -p revision="${RELEASE_VERSION}" \
+    -p url="${URL}" \
+    -w name=shared-workspace,volumeClaimTemplateFile=./tekton/volume-claim-template.yml
 
 tkn -n ${TARGET_NAMESPACE} pipeline logs cli-release-pipeline -f --last
