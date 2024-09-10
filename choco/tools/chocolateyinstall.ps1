f9931eec56f5bef5ee814f71730be6a8d6cc15c2
vinamra28 <vinjain@redhat.com>
2024-09-10 20:51:03 +0530

    Update docs for tkn 0.38.1
    Signed-off-by: vinamra28 <vinjain@redhat.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.38.0/tkn_0.38.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.38.1/tkn_0.38.1_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'tektoncd-cli*' 
-  checksum64     = '756414c0ac4245138e313c9662bc5960251ade6fb06cf58c95be88a8d8dc1c57'
+  checksum64     = '205bba1cccdb4da3670eff1eae960272959214b39563b0fb42a9fc5bb20c57c7'
   checksumType64 = 'sha256'
 }
 
