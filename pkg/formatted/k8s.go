e34ea22a1c7a05ba35631c831976ecf7481602dc
Chmouel Boudjnah <chmouel@redhat.com>
2024-11-27 16:39:43 +0100

    Do not print twice Cancelled when listing
    When listing the status of pipelinerun we would show something like
    this for cancelled PR:
    
    ```console
    NAME            STARTED         DURATION   STATUS
    pr-hmth-fmknk   5 minutes ago   1m8s       Succeeded
    pr-hmth-7qzlb   5 minutes ago   11s        Cancelled(Cancelled)
    pr-hmth-dtdrh   7 minutes ago   1m14s      Succeeded
    pr-zrbj-f4kq8   7 minutes ago   1m24s      Succeeded
    pr-zrbj-hxhw2   7 minutes ago   9s         Cancelled(Cancelled)
    ```
    
    which makes no sense, the Cancelled(Cancelled) is redundant, so we
    fix this by checking the reasons is not the same as what we are going to
    set. which now show a nicer:
    
    ```console
    NAME            STARTED          DURATION   STATUS
    pr-hmth-fmknk   8 minutes ago    1m8s       Succeeded
    pr-hmth-7qzlb   8 minutes ago    11s        Cancelled
    pr-hmth-dtdrh   10 minutes ago   1m14s      Succeeded
    pr-zrbj-f4kq8   10 minutes ago   1m24s      Succeeded
    pr-zrbj-hxhw2   10 minutes ago   9s         Cancelled
    ```
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- pkg/formatted/k8s.go
+++ pkg/formatted/k8s.go
@@ -72,10 +72,19 @@ func Condition(c v1.Conditions) string {
 	} else if c[0].Reason != "" && c[0].Reason != status {
 		switch c[0].Reason {
 		case "PipelineRunCancelled", "TaskRunCancelled", "Cancelled":
+			if c[0].Reason == "Cancelled" {
+				return ColorStatus("Cancelled")
+			}
 			return ColorStatus("Cancelled") + "(" + c[0].Reason + ")"
 		case "PipelineRunStopping", "TaskRunStopping":
+			if c[0].Reason == "Failed" {
+				return ColorStatus("Failed")
+			}
 			return ColorStatus("Failed") + "(" + c[0].Reason + ")"
 		case "CreateContainerConfigError", "ExceededNodeResources", "ExceededResourceQuota":
+			if c[0].Reason == "Pending" {
+				return ColorStatus("Pending")
+			}
 			return ColorStatus("Pending") + "(" + c[0].Reason + ")"
 		default:
 			return ColorStatus(status) + "(" + c[0].Reason + ")"
