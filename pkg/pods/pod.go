a30283ff4867247bf17e3f9914447cc6f03b5c59
divyansh42 <diagrawa@redhat.com>
2025-06-17 18:20:16 +0530

    Fix deadlock issue when following logs
    Signed-off-by: divyansh42 <diagrawa@redhat.com>

--- pkg/pods/pod.go
+++ pkg/pods/pod.go
@@ -69,6 +69,12 @@ func NewWithDefaults(name, ns string, client k8s.Interface) *Pod {
 	}
 }
 
+// podResult holds the result of pod status check
+type podResult struct {
+	pod *corev1.Pod
+	err error
+}
+
 // Wait wait for the pod to get up and running
 func (p *Pod) Wait() (*corev1.Pod, error) {
 	// ensure pod exists before we actually check for it
@@ -77,66 +83,62 @@ func (p *Pod) Wait() (*corev1.Pod, error) {
 	}
 
 	stopC := make(chan struct{})
-	eventC := make(chan interface{}, 10)
 	mu := sync.Mutex{}
-	defer func() {
-		mu.Lock()
-		close(stopC)
-		close(eventC)
-		mu.Unlock()
+
+	var result podResult
+
+	// Start watcher in a goroutine
+	go func() {
+		p.watcher(stopC, &result, &mu)
 	}()
 
-	p.watcher(stopC, eventC, &mu)
-
-	var pod *corev1.Pod
-	var err error
-	for e := range eventC {
-		pod, err = checkPodStatus(e)
-		if pod != nil || err != nil {
-			break
-		}
-	}
-
-	return pod, err
+	// Wait for stopC
+	<-stopC
+	return result.pod, result.err
 }
 
-func (p *Pod) watcher(stopC <-chan struct{}, eventC chan<- interface{}, mu *sync.Mutex) {
+func (p *Pod) watcher(stopC chan struct{}, result *podResult, mu *sync.Mutex) {
 	factory := informers.NewSharedInformerFactoryWithOptions(
 		p.Kc, time.Second*10,
 		informers.WithNamespace(p.Ns),
 		informers.WithTweakListOptions(podOpts(p.Name)))
 
+	updatePodStatus := func(obj interface{}) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		pod, err := checkPodStatus(obj)
+		if pod != nil || err != nil {
+			result.pod = pod
+			result.err = err
+			close(stopC)
+		}
+	}
+
 	_, err := factory.Core().V1().Pods().Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				mu.Lock()
-				defer mu.Unlock()
 				select {
 				case <-stopC:
 					return
 				default:
-					// default is used to avoid pseudo-random selection of multiple matching cases
-					eventC <- obj
+					updatePodStatus(obj)
 				}
 			},
 			UpdateFunc: func(_, newObj interface{}) {
-				mu.Lock()
-				defer mu.Unlock()
 				select {
 				case <-stopC:
 					return
 				default:
-					eventC <- newObj
+					updatePodStatus(newObj)
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
-				mu.Lock()
-				defer mu.Unlock()
 				select {
 				case <-stopC:
 					return
 				default:
-					eventC <- obj
+					updatePodStatus(obj)
 				}
 			},
 		})
