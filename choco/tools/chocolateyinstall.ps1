c6e402a81a66692ee77eb4842711765a53e7641f
pratap0007 <shverma@redhat.com>
2026-08-26 12:48:52 +0530

    chore(docs): update version to v0.46.0
    Update version references across documentation and release files:
    - Update README download links from v0.45.0 to v0.46.0
    - Update goreleaser image from v2.17.1 to v2.18.0
    
    Signed-off-by: pratap0007 <shverma@redhat.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
-﻿$ErrorActionPreference = 'Stop'; 
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'tektoncd-cli'
-$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.44.0/tkn_0.44.0_Windows_x86_64.zip'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.46.0/tkn_0.46.0_Windows_x86_64.zip'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
-  softwareName   = 'tektoncd-cli*' 
-  checksum64     = 'ad85940171d3c75b6161956b5080741d067398614405831cb0f9654667867c9e'
+  softwareName   = 'tektoncd-cli*'
+  checksum64     = 'aea8a3d5bdf3bec966501cf0d7655c7c9a13cb4e1ad53f5235db5a1375960d0c '
   checksumType64 = 'sha256'
 }
 
